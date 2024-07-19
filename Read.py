a91c2055b2b65af674a6a35dbb51ebfa22582fc7
shinadots <112908866+shinadots@users.noreply.github.com>
2024-07-19 10:55:53 -0300

    Update Read.py

--- Read.py
+++ Read.py
@@ -5,3 +5,8 @@ import pandas as pd
 def ler_dados_era5(caminho_arquivo):
     df = pd.read_parquet(caminho_arquivo)
     return df
+def exportar_para_csv(df, arquivo_saida):
+    df.to_csv(arquivo_saida, index=False)
+    
+def exportar_para_excel(df, arquivo_saida):
+    df.to_excel(arquivo_saida, index=False)
