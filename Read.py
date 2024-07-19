3f90f14c6145f1e795c95dc3b9b6ae37f61a7b76
shinadots <112908866+shinadots@users.noreply.github.com>
2024-07-19 10:56:07 -0300

    Update Read.py

--- Read.py
+++ Read.py
@@ -10,3 +10,19 @@ def exportar_para_csv(df, arquivo_saida):
     
 def exportar_para_excel(df, arquivo_saida):
     df.to_excel(arquivo_saida, index=False)
+def valores_negativos(val):
+    cor = 'red' if val < 0 else 'white'
+    return f'color: {cor}'
+
+def exibir_dados_com_estilo(df):
+    styled_df = df.style.applymap(valores_negativos, subset=['u10'])
+    return styled_df
+
+def agrupar_dados(df, modo='Diário'):
+    if modo == 'Semanal':
+        df['data'] = pd.to_datetime(df['data'])
+        df = df.resample('W-Mon', on='data').mean().reset_index().sort_values(by='data')
+    elif modo == 'Diário':
+        pass 
+        
+    return df
